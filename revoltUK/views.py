ad237294ac9481b57e32de968d5433c09c3dc59f
Logan Harris - Ultrane Development <logan.harris@ultranedevelopment.co.uk>
2024-09-23 11:59:33 +0100

    Update
    - Entire Voting System Completed & Implemented

--- revoltUK/views.py
+++ revoltUK/views.py
@@ -1,26 +1,40 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Legislation, Category, VotingSession
+from .models import Legislation, Category, VotingSession, Vote
 from django.utils import timezone
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from .models import VotingSession, Vote
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.db.models import Avg
 
 def home(request):
     categories = Category.objects.all()
     current_time = timezone.now()
 
-    # Fetch active voting sessions
+    # Fetch active and closed voting sessions
     active_sessions = VotingSession.objects.filter(start_time__lte=current_time, end_time__gte=current_time)
-
-    # Fetch closed voting sessions
     closed_sessions = VotingSession.objects.filter(end_time__lt=current_time)
+
+    # Calculate ratings for each session
+    session_ratings = {}
+    for session in active_sessions:
+        votes = Vote.objects.filter(session=session)
+        total_votes = votes.count()
+        print(f"Session ID: {session.id}, Total Votes: {total_votes}")  # Debug output
+
+        if total_votes > 0:
+            avg_rating = votes.aggregate(Avg('rating'))['rating__avg']
+            session_ratings[session.id] = avg_rating
+        else:
+            session_ratings[session.id] = 0  # No votes, set rating to 0
+
+    print(f"Session Ratings: {session_ratings}")  # Debug output
 
     return render(request, 'index.html', {
         'categories': categories,
         'active_sessions': active_sessions,
         'closed_sessions': closed_sessions,
+        'session_ratings': session_ratings,  # Pass session ratings
     })
 
 def signup_view(request):
